4378c0739de8858266ec62cc3927063eebc86a33
waye <chengwh2024@shanghaitech.edu.cn>
2026-03-26 00:51:47 +0800

    delete .env

--- run.bat
+++
@@ -1,7 +0,0 @@
-python -m src.port_pipeline.scan_cli --place "singapore" --size 1280x1280 --meters 2000 --detector-format json --mmrotate-config mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90.py --mmrotate-checkpoint mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth --output-dir outputs_scan_5000 --verbose --max-no-new 3 --step-sleep-seconds 0.2
-
-
-python -m src.port_pipeline.scan_cli --place "singapore" --size 960x960 --meters 2000 --detector-format json --mmrotate-config mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90.py --mmrotate-checkpoint mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth --output-dir outputs_scan_2000 --verbose --max-no-new 5 --step-sleep-seconds 0.2
-
-
-python -m src.port_pipeline.scan_cli --place "singapore" --size 960x960 --meters 2000 --detector-format json --mmrotate-config mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90.py --mmrotate-checkpoint mmrotate/oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth --output-dir outputs_scan_2000 --verbose --max-no-new 5 --step-sleep-seconds 0.6
